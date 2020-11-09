a744ecedc98c4e44af1a4c2b6166316caf272942
emilia-szymanska <emiliaszym@gmail.com>
2020-11-09 01:48:33 +0100

    Improve client file

--- client.py
+++ client.py
@@ -3,58 +3,32 @@
 import socket
 import numpy
 import time
+import numpy as np
+import sys
+import struct
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 8080        # The port used by the server
 
-x = numpy.array([0, 0, 0])
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    x = x.tobytes()
-    s.sendall(x)
-    data = s.recv(1024)
-    print('Received', repr(data))
-    time.sleep(3)
-    s.sendall(x)
-    data = s.recv(1024)
-    print('Received', repr(data))
-    s.sendall(b'end')
 
 
-#!/usr/bin/python3
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Wrong number of arguments. Run as './main.py time number_of_signals' ")
+    else:
+        t = int(sys.argv[1])
+        n = int(sys.argv[2])
 
-#import http.server
-#import socketserver
-#import time
-#import numpy as np
-#import sys
-
-#if __name__ == "__main__":
-#    if len(sys.argv) != 3:
-#        print("Wrong number of arguments. Run as './main.py time number_of_signals' ")
- #   else:
-#        t = int(sys.argv[1])
-#        n = int(sys.argv[2])
-
-#        beginning = np.array([0, 0, 0])
+        beginning = np.array([0, 0, 0])
         
-#        previous = beginning
-#        for i in range(n):
-#            current = np.random.randint(-30, high=30, size=3)
-#            dist = np.linalg.norm(previous - current)
-#            vel = dist / t
-#            print(vel)
-#            print(current)
-#            time.sleep(t)
-#            previous = current
-
-
-
-#PORT = 8080
-#handler = http.server.SimpleHTTPRequestHandler
-
-#with socketserver.TCPServer(("", PORT), handler) as httpd:
-#    print("serving at port ", PORT)
-#    httpd.serve_forever()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        for i in range(n):
+            current = np.random.randint(1, high=30, size=3)
+            current = current.tobytes()
+            s.sendall(current)
+            data = s.recv(1024)
+            print('Received', struct.unpack('f', data))
+            time.sleep(t)
+        s.sendall(b'end')
 
--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import numpy as np
 import socket
 import sys
+import struct
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 8080        # Port to listen on (non-privileged ports are > 1023)
@@ -13,6 +14,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
+        previous = np.array([0, 0, 0])
         while True:
             data = conn.recv(1024)
             data = data.decode()
@@ -20,16 +22,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if data == "end":
                 print("ending")
                 sys.exit(0)
-            arr = np.fromstring(data, dtype = np.int)
+            current = np.fromstring(data, dtype = np.int)
+            dist = np.linalg.norm(previous - current)
+            vel = dist / 3
             #print(arr)
-            arr = arr.tobytes()
                 #if not data:
                 #    pass
                 #else:
                 #print(arr)
             #if not data:
             #    break
-            conn.sendall(arr)
+            conn.sendall(struct.pack('f', vel))
+            previous = current
                 
 
 
+        
+#        previous = beginning
+#            print(current)
+#            time.sleep(t)
+#            previous = current
